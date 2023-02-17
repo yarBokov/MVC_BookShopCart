928492d95c4bc7eba1f2fd8a10cd85691e65b55d
yarBokov <bokovjaroslav@gmail.com>
2023-02-17 16:12:15 +0300

    Добавьте файлы проекта.

---
+++ BookShoppingCartMvcUi/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookShoppingCartMvcUi.Models
+{
+    [Table("Book")]
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        public string? BookName { get; set; }
+        [Required]
+        public int GenreId { get; set; }
+        public Genre genre;
+    }
+}
---
+++ BookShoppingCartMvcUi/Models/ShoppingCart.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookShoppingCartMvcUi.Models
+{
+    [Table("ShoppingCart")]
+    public class ShoppingCart
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string UsedId { get; set;}
+        public bool IsDeleted { get; set; } = false;
+    }
+}
