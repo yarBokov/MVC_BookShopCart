0e033dfae882f0de798c17c9860da769056367a8
yarBokov <bokovjaroslav@gmail.com>
2023-02-18 18:17:07 +0300

    Worked with Register part + Roles

---
+++ BookShoppingCartMvcUi/Data/DbSeeder.cs
@@ -0,0 +1,30 @@
+﻿using BookShoppingCartMvcUi.Constants;
+using Microsoft.AspNetCore.Identity;
+
+namespace BookShoppingCartMvcUi.Data
+{
+    public class DbSeeder
+    {
+        public static async Task SeedDefaultData(IServiceProvider serviceProvider)
+        {
+            var userMgr = serviceProvider.GetService<UserManager<IdentityUser>>();
+            var roleMgr = serviceProvider.GetService<RoleManager<IdentityRole>>();
+            await roleMgr.CreateAsync(new IdentityRole(Roles.Admin.ToString()));
+            await roleMgr.CreateAsync(new IdentityRole(Roles.User.ToString()));
+
+            var admin = new IdentityUser
+            {
+                UserName = "admin@gmail.com",
+                Email = "admin@gmail.com",
+                EmailConfirmed = true,
+            };
+
+            var userInDb = await userMgr.FindByEmailAsync(admin.Email);
+            if (userInDb is null)
+            {
+                await userMgr.CreateAsync(admin, "Admin123");
+                await userMgr.AddToRoleAsync(admin,Roles.Admin.ToString());
+            }
+        }
+    }
+}
--- BookShoppingCartMvcUi/Models/OrderStatus.cs
+++ BookShoppingCartMvcUi/Models/OrderStatus.cs
@@ -7,6 +7,8 @@ namespace BookShoppingCartMvcUi.Models
     public class OrderStatus
     {
         public int Id { get; set; }
+        [Required]
+        public int StatusId { get; set; }
         [Required, MaxLength(20)]
         public string? StatusName { get; set; }
     }
